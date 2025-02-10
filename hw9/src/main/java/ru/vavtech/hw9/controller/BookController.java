ca3ed39b65e0286a6f4a579eaf16b7aa2b6ee259
av <keyjin88@gmail.com>
2025-02-10 22:01:09 +0300

    Добавил редактирование и удаление книг

--- hw9/src/main/java/ru/vavtech/hw9/controller/BookController.java
+++ hw9/src/main/java/ru/vavtech/hw9/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.vavtech.hw9.controller.request.CreateBookRequest;
 import ru.vavtech.hw9.models.dto.BookDto;
@@ -46,6 +47,33 @@ public class BookController {
             return "add-book";
         }
         bookService.create(request.getTitle(), request.getAuthorId(), request.getGenreId());
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditBookForm(@PathVariable Long id, Model model) {
+        var book = BookDto.fromDomainObject(bookService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book ID: " + id)));
+        model.addAttribute("book", book);
+        model.addAttribute("authors", authorService.findAll());
+        model.addAttribute("genres", genreService.findAll());
+        return "edit-book";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateBook(@PathVariable Long id, @Valid BookDto bookDto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("authors", authorService.findAll());
+            model.addAttribute("genres", genreService.findAll());
+            return "edit-book";
+        }
+        bookService.update(id, bookDto.getTitle(), bookDto.getAuthor().getId(), bookDto.getGenre().getId());
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
         return "redirect:/";
     }
 
