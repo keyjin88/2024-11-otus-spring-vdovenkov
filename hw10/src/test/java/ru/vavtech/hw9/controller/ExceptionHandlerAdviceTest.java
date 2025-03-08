a80db7290474fffc813ef2b53f94804a5a088c5f
AV <keyjin88@gmail.com>
2025-03-08 21:52:21 +0300

    rename group

--- hw10/src/test/java/ru/vavtech/hw9/controller/ExceptionHandlerAdviceTest.java
+++ hw10/src/test/java/ru/vavtech/hw9/controller/ExceptionHandlerAdviceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.vavtech.hw10.controller.BookController;
 import ru.vavtech.hw10.exceptions.NotFoundException;
@@ -24,13 +24,13 @@ class ExceptionHandlerAdviceTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private BookService bookService;
 
-    @MockBean
+    @MockitoBean
     private AuthorService authorService;
 
-    @MockBean
+    @MockitoBean
     private GenreService genreService;
 
     @DisplayName("Should return 404 page when entity not found")
