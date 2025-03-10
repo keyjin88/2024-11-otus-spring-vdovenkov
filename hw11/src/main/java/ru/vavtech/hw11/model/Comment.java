e0aa90323ac025297840a3fa7b537d4ee96b895b
AV <keyjin88@gmail.com>
2025-03-10 22:49:53 +0300

    Migrate Comment model and repository to MongoDB and Reactive Spring Data

--- hw11/src/main/java/ru/vavtech/hw11/model/Comment.java
+++ hw11/src/main/java/ru/vavtech/hw11/model/Comment.java
@@ -1,38 +1,24 @@
 package ru.vavtech.hw11.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "comments")
+@Document(collection = "comments")
 public class Comment {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private String id;
 
-    @Column(name = "text", nullable = false)
     private String text;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "book_id", nullable = false)
+    @DBRef
     private Book book;
-
-    public Comment(Book book) {
-        this.book = book;
-    }
 }
--- hw11/src/main/java/ru/vavtech/hw11/repository/CommentRepository.java
+++ hw11/src/main/java/ru/vavtech/hw11/repository/CommentRepository.java
@@ -1,11 +1,7 @@
 package ru.vavtech.hw11.repository;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import ru.vavtech.hw11.model.Comment;
 
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByBookId(long bookId);
+public interface CommentRepository extends ReactiveMongoRepository<Comment, String> {
 }
